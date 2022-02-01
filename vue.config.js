8a41dbc758f4f8d91591337a0c3f98fd705c407a
penghui <penghui_2053@163.com>
2022-02-01 22:39:39 +0800

    添加检测循环引用的插件

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+// 检测循环引用的插件
+const circular = require('circular-dependency-plugin');
 
 module.exports = {
   chainWebpack(config){
     config.plugin('monaco').use(new MonacoWebpackPlugin())
+    config.plugin('circular').use(new circular())
   },
 }
