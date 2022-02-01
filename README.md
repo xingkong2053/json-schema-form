58073bf390a0be7d7f5f788854c05fc8a6f806b3
penghui <penghui_2053@163.com>
2022-02-01 16:55:35 +0800

    项目初始化

--- README.md
+++ README.md
@@ -22,3 +22,111 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### vue3定义组件
+
+```vue
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+});
+</script>
+```
+
+### 使用jsx
+
+1. 安装plugin
+```shell
+yarn add @vue/babel-plugin-jsx -D
+```
+
+2. 在babel.config.js 中添加插件
+```js
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: ['@vue/babel-plugin-jsx'],
+};
+```
+
+3. `jsx`组件文件格式
+
+   ```tsx
+   import { defineComponent, reactive } from "vue";
+   
+   export default defineComponent({
+     setup(){
+   
+       const state = reactive({
+         name: 'world'
+       })
+   
+       return ()=>{
+         const { name } = state;
+   
+         return <div>
+           hello {name}
+         </div>
+       }
+     }
+   })
+   ```
+
+   ### 安装ajv
+
+   ```shell
+   yarn add ajv ajv-formats ajv-i18n ajv-errors -S
+   ```
+
+   ### 安装
+
+   ```shell
+   yarn add monaco-editor@^0.21.2 monaco-editor-webpack-plugin@^2.0.0 -D
+   ```
+
+   ### 安装`vue-jss`
+
+   ```shell
+   yarn add vue-jss jss jss-preset-default -S
+   ```
+
+   ### eslint.config.js
+
+   ```json
+     "eslintConfig": {
+       "root": true,
+       "env": {
+         "node": true
+       },
+       "extends": [
+         "plugin:vue/vue3-essential",
+         "eslint:recommended",
+         "@vue/typescript/recommended",
+         "@vue/prettier",
+         "@vue/prettier/@typescript-eslint"
+       ],
+       "parserOptions": {
+         "ecmaVersion": 2020
+       },
+       "rules": {}
+     },
+   ```
+
+   ### 添加支持可选链操作符
+
+   ```shell
+   yarn add @babel/plugin-proposal-optional-chaining -D
+   ```
+
+   ```.js
+   // .babelrc
+   {
+       "plugins": ["@babel/plugin-proposal-optional-chaining"]
+   }
+   ```
+
+   
---
+++ src/App.tsx
@@ -0,0 +1,179 @@
+import { defineComponent, reactive, ref, Ref, watchEffect } from "vue";
+import MonacoEditor from "./components/MonacoEditor";
+import { createUseStyles } from "vue-jss";
+import demos from "./demos";
+import { ElSelect, ElOption } from "element-plus";
+
+
+const useStyles = createUseStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: '1200px',
+    margin: '0 auto',
+  },
+  menu: {
+    marginBottom: 20,
+  },
+  code: {
+    width: 700,
+    flexShrink: 0,
+  },
+  codePanel: {
+    minHeight: 400,
+    marginBottom: 20,
+  },
+  uiAndValue: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    '& > *': {
+      width: '46%',
+    },
+  },
+  content: {
+    display: 'flex',
+  },
+  form: {
+    padding: '0 20px',
+    flexGrow: 1,
+  },
+  menuButton: {
+    appearance: 'none',
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+    display: 'inline-block',
+    padding: 15,
+    borderRadius: 5,
+    '&:hover': {
+      background: '#efefef',
+    },
+  },
+  menuSelected: {
+    background: '#337ab7',
+    color: '#fff',
+    '&:hover': {
+      background: '#337ab7',
+    },
+  },
+})
+
+
+function toJson(data: any){
+  return JSON.stringify(data,null, 2)
+}
+
+type Schema = any
+
+type UISchema = any
+
+type Demo = {
+  schema: Schema | null
+  data: any
+  uiSchema: UISchema | null
+  schemaCode: string
+  dataCode: string
+  uiSchemaCode: string
+  customValidate: ((d: any, e: any) => void) | undefined
+}
+
+export default defineComponent({
+
+  setup(){
+
+    //使用的demo序号
+    const selectedRef: Ref<number> = ref(0)
+
+    const demo: Demo = reactive({
+      schema: null,
+      data: {},
+      uiSchema: {},
+      schemaCode: '',
+      dataCode: '',
+      uiSchemaCode: '',
+      customValidate: undefined
+    })
+
+    // 当select序号变化时，更新对应的值
+    watchEffect(()=>{
+      const index = selectedRef.value;
+      const d = demos[index];
+      demo.schema = d.schema
+      demo.data = d.default
+      demo.uiSchema = d.uiSchema
+      demo.schemaCode = toJson(d.schema)
+      demo.dataCode = toJson(d.default)
+      demo.uiSchemaCode = toJson(d.uiSchema)
+    })
+
+    const classesRef = useStyles()
+
+    // closure 闭包 demo
+    function handleCodeChange(
+      field: 'schema' | 'data' | 'uiSchema',
+      value: string,
+    ) {
+      try {
+        demo[field] = JSON.parse(value)
+        ;(demo as any)[`${field}Code`] = value
+      } catch (err) {
+        // some thing
+      }
+    }
+
+    const handleSchemaChange = (v: string) => handleCodeChange('schema', v)
+    const handleDataChange = (v: string) => handleCodeChange('data', v)
+    const handleUISchemaChange = (v: string) => handleCodeChange('uiSchema', v)
+
+    return ()=>{
+
+      const classes = classesRef.value
+      const selected = selectedRef.value
+
+      return <div>
+        <div class={classes.container}>
+          <div class={classes.menu}>
+            <h1>Vue3 JsonSchema Form</h1>
+            {/* 切换按钮 */}
+            <ElSelect v-model={selected}  onChange={(value)=>selectedRef.value = value} >
+              {
+                demos.map(({name},index)=> <ElOption value={index} key={index} label={name}/>)
+              }
+            </ElSelect>
+          </div>
+          {/* /.menu */}
+          <div class={classes.content}>
+            <div class={classes.code}>
+              <MonacoEditor
+                code={demo.schemaCode}
+                class={classes.codePanel}
+                onChange={handleSchemaChange}
+                title="Schema"
+              />
+              <div class={classes.uiAndValue}>
+                <MonacoEditor
+                  code={demo.uiSchemaCode}
+                  class={classes.codePanel}
+                  onChange={handleUISchemaChange}
+                  title="UISchema"
+                />
+                <MonacoEditor
+                  code={demo.dataCode}
+                  class={classes.codePanel}
+                  onChange={handleDataChange}
+                  title="Value"
+                />
+              </div>
+              {/* /.uiAndValue */}
+            </div>
+            {/* /.code */}
+            <div class={classes.form}>
+              form
+            </div>
+          </div>
+        </div>
+      </div>
+    }
+  }
+})
